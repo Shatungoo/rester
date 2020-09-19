c30bb76a612878dfd7e8d228cd1bcd9caeaaf781
shatungoo <shatungoo@gmail.com>
2020-09-20 00:55:28 +0300

    update to java 15

--- src/main/java/com/helldaisy/Request.java
+++ src/main/java/com/helldaisy/Request.java
@@ -7,36 +7,46 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.HashMap;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class Request {
 
-    @FXML
-    public TextField urlField;
+    @FXML public TextField urlField;
 
-    @FXML
-    public TextArea responseField;
+    @FXML public TextArea responseField;
 
-    @FXML
-    public ChoiceBox<String> method;
+    @FXML public ChoiceBox<String> method;
 
-    @FXML
-    public TextArea requestBody;
+    @FXML public TextArea requestBody;
 
-    @FXML
-    public TableView requestHeaders;
-    
+    @FXML public TableView<String> requestHeaders;
+    @FXML public  TableColumn<String, String> name;
+    @FXML public  TableColumn<String, String> value;
+
     @FXML
 public void initialize() {
+    //Deafult method
     method.getItems().addAll("GET", "POST", "PUT");
     method.getSelectionModel().select("GET");
+
+    // headers
+    final var map = new HashMap<String, String>();
+    map.put("key", "value");
+    requestHeaders.getItems().addAll(map.keySet());
+    
+    name.setCellValueFactory(cd -> new SimpleStringProperty(cd.getValue()));
+    value.setCellValueFactory(cd -> new SimpleStringProperty(map.get(cd.getValue())));
+    // requestHeaders.getColumns().addAll(keyColumn, valueColumn);
 }
 
     @FXML
