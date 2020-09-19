ac401727009ae5bc1398481f900dce1c292b653f
shatungoo <shatungoo@gmail.com>
2020-09-20 01:27:07 +0300

    add support of headers

--- src/main/java/com/helldaisy/Request.java
+++ src/main/java/com/helldaisy/Request.java
@@ -14,58 +14,72 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.TextFieldTableCell;
 
 public class Request {
 
-    @FXML public TextField urlField;
-
-    @FXML public TextArea responseField;
-
-    @FXML public ChoiceBox<String> method;
-
-    @FXML public TextArea requestBody;
-
-    @FXML public TableView<String> requestHeaders;
-    @FXML public  TableColumn<String, String> name;
-    @FXML public  TableColumn<String, String> value;
+    @FXML
+    public TextField urlField;
 
     @FXML
-public void initialize() {
-    //Deafult method
-    method.getItems().addAll("GET", "POST", "PUT");
-    method.getSelectionModel().select("GET");
+    public TextArea responseField;
 
-    // headers
-    final var map = new HashMap<String, String>();
-    map.put("key", "value");
-    requestHeaders.getItems().addAll(map.keySet());
-    
-    name.setCellValueFactory(cd -> new SimpleStringProperty(cd.getValue()));
-    value.setCellValueFactory(cd -> new SimpleStringProperty(map.get(cd.getValue())));
-    // requestHeaders.getColumns().addAll(keyColumn, valueColumn);
-}
+    @FXML
+    public ChoiceBox<String> method;
+
+    @FXML
+    public TextArea requestBody;
+
+    @FXML
+    public TableView<String> requestHeaders;
+    @FXML
+    public TableColumn<String, String> name;
+    @FXML
+    public TableColumn<String, String> value;
+
+    final HashMap<String, String> headers = new HashMap<>();
+
+    @FXML
+    public void initialize() {
+        // Deafult method
+        method.getItems().addAll("GET", "POST", "PUT");
+        method.getSelectionModel().select("GET");
+
+        // headers
+        headers.put("key", "value");
+        requestHeaders.getItems().addAll(headers.keySet());
+
+        name.setCellValueFactory(cd -> new SimpleStringProperty(cd.getValue()));
+        value.setCellValueFactory(cd -> new SimpleStringProperty(headers.get(cd.getValue())));
+
+        requestHeaders.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        name.setCellFactory(TextFieldTableCell.forTableColumn());
+        value.setCellFactory(TextFieldTableCell.forTableColumn());
+    }
 
     @FXML
     public void sendRequest(final ActionEvent event) {
         final HttpClient client = HttpClient.newHttpClient();
-        final HttpRequest request = HttpRequest.newBuilder()
-            // .headers(headers)
-            .method(method.getValue(),
-                BodyPublishers.ofString(requestBody.getText()))
-            .uri(URI.create(urlField.getText())).build();
-        client.sendAsync(request, BodyHandlers.ofString())
-            .thenApply(HttpResponse::body)
-            .thenAccept((body)-> { responseField.setText(body);})
-            .join();
+        final var requestBuilder = HttpRequest.newBuilder()
+            .method(method.getValue(), BodyPublishers.ofString(requestBody.getText()))
+            .uri(URI.create(urlField.getText()));
+        headers.forEach((key, value) -> requestBuilder.header(key, value));
+        client.sendAsync(requestBuilder.build(), 
+            BodyHandlers
+                .ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept((body) -> responseField.setText(body))
+                .join();
         // try {
-        //     final var response = client.send(request, BodyHandlers.ofString());
-        //     responseField.setText(response.body());
+        // final var response = client.send(request, BodyHandlers.ofString());
+        // responseField.setText(response.body());
         // } catch (IOException| InterruptedException e) {
-        //     e.printStackTrace();
+        // e.printStackTrace();
         // }
     }
 
--- src/main/java/com/helldaisy/SimpleStringProperty.java
+++
@@ -1,5 +0,0 @@
-package com.helldaisy;
-
-public class SimpleStringProperty {
-
-}
